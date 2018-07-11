e8fa675c06ec46420a0d65bc1f6e6ef84a0543f7
Glitch (adalytics-live) <none>
2018-07-11 21:33:47 +0000

    Color coding :)

--- public/templates.js
+++ public/templates.js
@@ -2,9 +2,26 @@ import {wire} from '/node_modules/viperhtml/index.js';
 
 const tableState = {};
 
+const knownHosts = [];
+
+function urlToHSL(str) {
+  try {
+    str = new URL(str).hostname; 
+  } catch (e) {}
+  
+  let index = knownHosts.indexOf(str);
+  if (index === -1) index = knownHosts.push(str);
+  
+  return `hsl(${137.50 * index}deg, 100%, 30%)`;
+}
+
 const row = (data, lastupdated) => wire(tableState, ':' + encodeURIComponent(data.url))`
-  <tr class="${data.url === lastupdated ? 'highlighted' : ''}">
-    <td>${data.url}</td>
+  <tr class="${
+    data.url === lastupdated ? 'highlighted' : ''
+  }" style=${{
+    color: urlToHSL(data.url)
+  }}>
+    <td><a href="${data.url}" target="_blank" rel="nofollow">${data.url}</a></td>
     <td>${data.counter}</td>
   </tr>`;
 
